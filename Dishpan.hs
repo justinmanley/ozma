3684d90161f8e87636c003579d54b8b1bd260318
Justin Manley <manleyjster@gmail.com>
2014-03-23 11:04:00 -0500

    Let's be real: why was I using Haskell for all this text parsing, anyway?

--- Dishpan.hs
+++ Dishpan.hs
@@ -1,28 +1,83 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric  #-}
 module Dumpfile (parseDumpfile) where
 
 import Text.ParserCombinators.ReadP
 import Data.Char
+import System.IO (readFile)
+import Data.Aeson ((.:), (.:?), decode, FromJSON(..), Value(..), fromJSON)
+import Control.Applicative ((<$>), (<*>))
+import qualified Data.ByteString.Lazy.Char8 as C
+import GHC.Generics
+import Data.Aeson.Parser (json)
 
 data Dumpfile = Dumpfile {
 	unpack :: [[String]]
 }
 
-newline :: ReadP String
-newline = string "\n" <++ string "\r\n" <++ string "\r"
+data SurveyResponse = SurveyResponse Int String Timestamp
+	deriving Show
 
-tab :: ReadP String
-tab = string "\t"
+data PathData = PathData {
+	path :: [String],
+	timestamps :: [String]
+} deriving (Show, Generic)
 
-unique_id :: ReadP String
-unique_id = munch isDigit
+data LatLng = LatLng { lat, lng :: String } deriving (Show, Generic)
 
-isNotTab = (/= "\t")
+data Timestamp = Timestamp {
+	year :: Int,
+	month :: Int,
+	day :: Int,
+	hour :: Int,
+	minute :: Int,
+	second :: Int
+} deriving Show
 
-field = 	munch (\_ -> True)
+instance FromJSON PathData
+instance FromJSON LatLng
 
-instance Read Dumpfile where
-	readsPrec _ = readP_to_S $ fmap Dumpfile $ dumpfile_parse where
-		record = field `sepBy` tab
-		dumpfile_parse = record `endBy` newline
+parse :: ReadP a -> String -> a
+parse p s = case [a | (a,"") <- readP_to_S p s] of
+	[x] -> x
+	[] -> error "No parse."
+	_ -> error "Ambiguous parse."
 
-parseDumpfile = unpack . read
+parseDumpfile = parse dumpfile_parse where
+	dumpfile_parse = record `endBy` newline
+	record = do
+		id <- munch isNumber
+		tab
+		field -- unique identifier for user
+		tab
+		field --kmlstring
+		tab
+		jsonString <- jsonParse
+		tab
+		time <- timestamp
+		return $ SurveyResponse (read id) jsonString time
+	tab = char '\t'
+	field = munch (`notElem` "\t\n\r")
+	newline = string "\n" <++ string "\r\n" <++ string "\r"
+
+jsonParse = do
+	char '{'
+	str <- munch isAlpha
+	char '}'
+	return str
+
+timestamp = do
+	year <- munch isNumber
+	char '-'
+	month <- munch isNumber
+	char '-'
+	day <- munch isNumber
+	char ' '
+	hour <- munch isNumber
+	char ':'
+	minute <- munch isNumber
+	char ':' 
+	second <- munch isNumber
+	return $ Timestamp (read year) (read month) (read day) (read hour) (read minute) (read second)
+
+parsePathData :: String -> Maybe PathData
+parsePathData = (decode . C.pack)
